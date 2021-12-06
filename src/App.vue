a9d16c1ec9a2ca0fd5c92bbe023d0eb9b4590b37
Maggie Williams <maggie.williams@students.fhu.edu>
2021-12-05 20:56:00 -0600

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav>NAVIGATION</nav>
+
+  <PartyPokemon :partyPokemon="pp" @remove-pokemon="remove" />
+
+  <footer>&copy; 2021</footer>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import PartyPokemon from "./components/PartyPokemon.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PartyPokemon,
+  },
+  mounted() {
+    this.loadAllPokemon();
+  },
+
+  data() {
+    return {
+      pp: [
+        // {name:"Charmander", id: 3},
+        // {name:"Bulbasaur", id: 2},
+        // {name:"Pikachu", id: 4}
+      ],
+    };
+  },
+  methods: {
+    remove(id) {
+      this.pp = this.pp.filter((p) => p.id != id);
+    },
+
+    async loadAllPokemon() {
+      //load all pokemon from API
+      //load all pokemon from api and save into allPokemon
+      const getPokemon = async function (id) {
+        const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+        const response = await fetch(url);
+        const data = await response.json();
+
+        return data;
+      };
+      const pokemon_count = 151;
+      let pokemon = [];
+      for (let i = 1; i <= pokemon_count; i++) {
+        let p = await getPokemon(i);
+        if (p.name === "mr-mime") {
+          p.name = "Mr. Mime";
+        }
+      }
+      pokemon.forEach((p) => {
+        this.allPokemon.push(p);
+      });
+    },
+  },
+  
+};
 </script>
 
 <style>
---
+++ src/components/PartyPokemon.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="party">
+    <h2>Party</h2>
+    <h3>{{ partyPokemon.length }} of 6</h3>
+    <div id="party-pokemon">
+
+      <PokemonCard 
+        v-for="p in partyPokemon" 
+        :key="p.id" 
+        :pokemon="p" 
+        
+        @click-pokemon="remove"
+        />
+
+    </div>
+  </div>
+</template>
+
+<script>
+import PokemonCard from "./PokemonCard.vue"
+
+export default {
+  name: "PartyPokemon",
+  components: {
+    PokemonCard
+    },
+  props: {
+    partyPokemon: Array,
+  },
+  methods: {
+    remove(id) {
+      this.$emit("remove-pokemon", id);
+    }
+  }
+};
+</script>
+
+<style></style>
